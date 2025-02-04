9941af617b4da736ad00a0c8bd6e6163920172fa
andre <andreapereiradasilva27@gmail.com>
2025-02-04 20:59:21 -0300

    Obtendo post por id

---
+++ src/main/java/com/course/workshopmongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.course.workshopmongo.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.workshopmongo.domain.Post;
+import com.course.workshopmongo.repository.PostRepository;
+import com.course.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired 
+	private PostRepository repo; // # não recisa instanciar o objeto quando se usa o @Autowired
+	
+	
+
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null); // retorna nulo se não encontrar o id
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return user;
+	}
+	}
+		
+	
